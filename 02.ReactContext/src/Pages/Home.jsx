654ce4e0de601b209060e8441441b59a3d1eec1a
jesualujan <lujan.oficial@gmail.com>
2025-02-06 21:40:42 -0600

    ✨ creamos la vista de Home

--- 02.ReactContext/src/Pages/Home.jsx
+++ 02.ReactContext/src/Pages/Home.jsx
@@ -1,6 +1,18 @@
+import SongList from "../Components/SongList"
+import SongDetail from "../Components/SongDetail"
+import './home.css'
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="home-container">
+      <div className="left">
+        <p> lado izquierdo </p>
+        <SongList />
+      </div>
+      <div className="right">
+        <p>lado derecho</p>
+        <SongDetail />
+      </div>
+    </div>
   )
 }
 
