22f7e82b41fdc4157071ba57657b9f8a840e35e4
jesualujan <lujan.oficial@gmail.com>
2025-02-11 19:38:02 -0600

    ✨ creamos el consumidor

---
+++ 02.ReactContext/src/Hook/useSongContext.jsx
@@ -0,0 +1,17 @@
+//* 3) Crear un hook para usar o consummir el contexto de la lista de canciones.
+
+import { SongContext } from "../Context/SongContext";
+import { useContext } from "react";
+
+// hacer el export de una función
+
+export const useSongContext = () => {
+    // guardo mi contexto en una constante
+    const context = useContext(SongContext) // quiero usar mi contexto y esto lo guardo en context
+
+    // si no hay contexto, si es un undefined (vacío), lanzo un error
+   if(!context){
+        throw new Error('useSongContext  debe ser usado dentro de SongProvider')
+    } 
+    return context
+}
