bcd578cfcd23666c68d55b4768e7d7bf353d8e51
jesualujan <lujan.oficial@gmail.com>
2025-02-11 20:16:27 -0600

    ✨ agregamos un buscador al contexto

--- 02.ReactContext/src/Context/SongContext.jsx
+++ 02.ReactContext/src/Context/SongContext.jsx
@@ -1,8 +1,8 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect } from "react"
 import canciones from "../Utils/listaCanciones.json";
 
 //* 1) CREAR EL CONTEXTO 
-const SongContext = createContext(); // creamos el contexto y mi contexto empieza vacío
+const SongContext = createContext() // creamos el contexto y mi contexto empieza vacío
 
 //* 2) CREAR EL PROVIDER
 // EL PROVIDER ES UN COMPONENTE QUE ENVUELVE A TODOS LOS COMPONENTES 
@@ -13,27 +13,34 @@ function SongProvider ({children}) {
     const [list, setList] = useState([]); // estado que va a contener la lista de canciones
     const [loading, setLoading] = useState(true); // ¿está cargando la lista de canciones?
     const [selectedSong, setSelectedSong] = useState({}); // estado que va a contener la cancion seleccionada
-
+    const [search, setSearch] = useState('') // identifica la palabara que pongo en el buscador
+    
     // simulamos la llamada a la API
     useEffect(() => {
         setTimeout(() => {
-            setList(canciones)
-            setLoading(false) // cambio a false ya están cargando las canciones
+          setList(canciones)
+          setLoading(false) // cambia a false xk ya están cargando las canciones
         }, 2000)
-    },[])
+      }, [])
 
-    const data = {
-        list, 
-        loading, 
-        selectedSong, 
-        setSelectedSong
-    }
+      //mandamos un objeto
+      const data = {
+        list,
+        loading,
+        selectedSong,
+        setSelectedSong,
+        search,
+        setSearch
+      }
+      
     // el proveedor (provider) es un componente que envuelve a otros componentes
     // y le pasa un objeto con los datos que queremos compartir
+
     return (
-        <SongContext.Provider value={data}>
-            {children}
-        </SongContext.Provider>
+        // return de un componente
+    <SongContext.Provider value={data}>
+      {children}
+    </SongContext.Provider>
     )
 }
 
