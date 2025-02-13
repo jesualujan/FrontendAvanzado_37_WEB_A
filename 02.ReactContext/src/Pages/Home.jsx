f319db4b9dc0ba7d7a311a065611c33044e71258
jesualujan <lujan.oficial@gmail.com>
2025-02-12 22:35:01 -0600

    ✨ Importe Header en Home

--- 02.ReactContext/src/Pages/Home.jsx
+++ 02.ReactContext/src/Pages/Home.jsx
@@ -2,9 +2,11 @@ import SongList from "../Components/SongList"
 import SongDetail from "../Components/SongDetail"
 import { SongProvider } from "../Context/SongContext"
 import './home.css'
+import Header from "../Components/Header"
 const Home = () => {
   return (
     <SongProvider>
+      <Header />
     <div className='home-container'>
       <div className='left'>
         <p>Song List</p>
