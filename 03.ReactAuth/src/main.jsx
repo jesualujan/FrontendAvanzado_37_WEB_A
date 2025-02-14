e51f89170dbcf1b8d0bbfb6e5a69ca945c262618
jesualujan <lujan.oficial@gmail.com>
2025-02-13 20:13:52 -0600

    ✨ importe bootstrap

--- 03.ReactAuth/src/main.jsx
+++ 03.ReactAuth/src/main.jsx
@@ -1,7 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+// IMPORT DE BOOTSTRAP
+// BOOTSTRAP CSS
+import 'bootstrap/dist/css/bootstrap.min.css'
+// BOOTSTRAP JS
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
