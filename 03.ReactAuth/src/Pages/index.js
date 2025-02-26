f154f0fcdc088fc3e0899d0e0d14fbfd3b374a6b
jesualujan <lujan.oficial@gmail.com>
2025-02-25 19:10:27 -0600

    ✨ creación de las vistas

---
+++ 03.ReactAuth/src/Pages/index.js
@@ -0,0 +1,15 @@
+import Dashboard from './Dashboard'
+import Home from './Home'
+import Login from './Login'
+import Secret from './Secret'
+import Signup from './Signup'
+
+// Exportamos los componentes para que puedan ser importados en el archivo de rutas
+// exportamos como objeto
+export {
+    Dashboard,
+    Home,
+    Login,
+    Secret,
+    Signup
+}
