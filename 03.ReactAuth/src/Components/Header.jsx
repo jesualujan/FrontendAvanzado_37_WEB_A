f4f08f8cfc1a9f1e4f1344a9720b2f6547fc4fd6
jesualujan <lujan.oficial@gmail.com>
2025-02-27 21:02:02 -0600

    ✨ aplicamos un renderizado condicional

--- 03.ReactAuth/src/Components/Header.jsx
+++ 03.ReactAuth/src/Components/Header.jsx
@@ -1,7 +1,12 @@
 import './header.scss'
 import { NavLink } from 'react-router-dom'
+import { useAuthContext } from '@/Hook/useAuthContext'
 
 const Header = () => {
+  
+  const {logout, isAuth} = useAuthContext();
+
+
   const linkIsActive = (isActive) => isActive ? "header__item-link header__item-link--is-active" : "header__item-link"
 
   return (
@@ -17,15 +22,27 @@ const Header = () => {
         <li className="header__list-item">
             <NavLink className={({ isActive }) => linkIsActive(isActive)} to="/dashboard">Dashboard</NavLink>
         </li>
-        <li className="header__list-item">
+        {isAuth 
+         ?(
+          <>
+           <li className="header__list-item">
             <NavLink className={({ isActive }) => linkIsActive(isActive)} to="/secret">Secret</NavLink>
-        </li>
+          </li>
+          <li className="header__list-item">
+            <NavLink className="header__item-link" onClick={logout}>Logout</NavLink>
+         </li>
+          </>
+         )
+         : (
+           <> 
         <li className="header__list-item">
             <NavLink className={({ isActive }) => linkIsActive(isActive)} to="/login">Login</NavLink>
         </li>
         <li className="header__list-item">
             <NavLink className={({ isActive }) => linkIsActive(isActive)} to="/signup">Signup</NavLink>
         </li>
+           </>
+         )}
       </nav>
     </div>
   )
