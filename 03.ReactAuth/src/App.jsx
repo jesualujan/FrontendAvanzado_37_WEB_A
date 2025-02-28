8537ca0c1c7354a7e9fafa2dfb1510c9b333cf7c
jesualujan <lujan.oficial@gmail.com>
2025-02-27 20:42:16 -0600

    ✨envolvemos a nuestros componentes que consumen el contexto

--- 03.ReactAuth/src/App.jsx
+++ 03.ReactAuth/src/App.jsx
@@ -2,15 +2,17 @@ import Header from '@/Components/Header'
 import './App.css'
 import RoutesIndex from '@/Routes/RoutesIndex'
 import { BrowserRouter } from 'react-router-dom'
-
+import { AuthProvider } from '@/Context/AuthContext'
 function App() {
 
   return (
     <>
+    <AuthProvider>
       <BrowserRouter>
         <Header />
         <RoutesIndex />
       </BrowserRouter>
+    </AuthProvider>
     </>
   )
 }
