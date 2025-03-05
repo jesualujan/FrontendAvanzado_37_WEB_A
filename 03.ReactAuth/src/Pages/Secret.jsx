4427acad59b2630d32e9b0ee3a1fbca679c320ac
jesualujan <lujan.oficial@gmail.com>
2025-03-04 19:12:00 -0600

    ✨ mostramos el tipo de perfil si es admin o customer

--- 03.ReactAuth/src/Pages/Secret.jsx
+++ 03.ReactAuth/src/Pages/Secret.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+// Definir el perfil de un usuario
+// tipo: Admin 
+// tipo: Customer
+import { useAuthContext } from "@/Hook/useAuthContext"
+
 
 const Secret = () => {
+  const { userPayload } = useAuthContext() // consumimos el contexto
   return (
-    <div>Secret</div>
+    <>
+    <h1> Tipo de usuario 🧑‍🦰</h1>
+    {
+      userPayload?.role === 'ADMIN'
+      ? <h2> Hola Admin! bienvenido a tu perfil 🫲</h2> // lo pueden reemplazar por una card
+      : <h2> Hola Customer! bienvenido a tu perfil 🫲</h2> // lo pueden reemplazar por una card
+    }
+    { userPayload?.role === 'ADMIN' && <h3> Saludos Admin 🫲</h3> }
+    { userPayload?.role === 'CUSTOMER' && <h3> Saludos Customer 🫲</h3> }
+    </>
   )
 }
 
